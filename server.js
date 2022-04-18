e03414f7446f303abf34d71803ad229335572018
Kalyan Kalapala <kalyankalapala@Kalyans-Air-2.lan>
2022-04-18 18:51:59 -0500

    kalyan kalapala - UCM

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">sachin</h1> \n');
+  res.send('<h1 style="color:green;">kalyan kalapala - UCM</h1> \n');
 });
 
 app.listen(PORT, HOST);
