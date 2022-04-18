041f0a38096004d2864cd5e08473810acaa86710
Kalyan Kalapala <kalyankalapala@Kalyans-Air-2.lan>
2022-04-18 15:48:44 -0500

    best changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">Kalyan App</h1> \n');
+  res.send('<h1 style="color:green;">Kalyan kalapala</h1> \n');
 });
 
 app.listen(PORT, HOST);
